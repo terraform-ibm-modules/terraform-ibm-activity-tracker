1a01409a8c54e8f7b4501ecd31a3c190bfbea6b3
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-23 20:03:32 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v9

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -91,7 +91,7 @@ module "key_protect" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "8.21.8"
+  version           = "9.0.2"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -104,7 +104,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "8.21.8"
+  version = "9.0.2"
   bucket_configs = [
     {
       bucket_name                   = local.at_bucket_name
