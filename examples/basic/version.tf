ca712d0de1d38570a281ec6209548513925501bd
Md Anam Raihan <md.anam.raihan17@gmail.com>
2025-07-03 18:21:15 +0530

    test: Update provider version and test source (#43)
    * update version and test
    
    * add comment
    
    ---------
    
    Co-authored-by: Md Anam Raihan <md.anam.raihan@ibm.com>

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.79.0"
+      version = "1.79.2" # Locking into 1.79.2 due to https://github.com/terraform-ibm-modules/terraform-ibm-activity-tracker/pull/43#issuecomment-3027718565
     }
   }
 }
