2a40da9866c339902e67c3ec4119c56cf064ba35
Md Anam Raihan <md.anam.raihan17@gmail.com>
2025-08-18 14:23:33 +0530

    test: removed region "in-che" from test as Event Streams does not support this region (#82)
    * remove in-che from valid region
    
    * update comment
    
    ---------
    
    Co-authored-by: Md Anam Raihan <md.anam.raihan@ibm.com>

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -26,8 +26,8 @@ const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-res
 const fullyConfigurableTerraformDir = "solutions/fully-configurable"
 const AccountSettingsDADir = "solutions/event-routing-account-settings"
 
+// Removed "in-che" from validRegions since it isn’t supported under the Standard plan of Event Streams.
 var validRegions = []string{
-	"in-che",
 	"au-syd",
 	"br-sao",
 	"ca-tor",
