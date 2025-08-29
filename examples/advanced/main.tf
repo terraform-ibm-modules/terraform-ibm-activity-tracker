c3c3644eb97f8ae8b8d0879ed380794fd8209697
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-29 19:44:15 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -39,7 +39,7 @@ locals {
 
 module "event_streams" {
   source            = "terraform-ibm-modules/event-streams/ibm"
-  version           = "4.0.26"
+  version           = "4.0.27"
   es_name           = "${var.prefix}-eventsteams-instance"
   tags              = var.resource_tags
   region            = var.region
@@ -91,7 +91,7 @@ module "key_protect" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.2.6"
+  version           = "10.2.7"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -104,7 +104,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.2.6"
+  version = "10.2.7"
   bucket_configs = [
     {
       bucket_name                   = local.at_bucket_name
