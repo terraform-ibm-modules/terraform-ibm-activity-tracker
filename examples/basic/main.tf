64a9bf96124492bd64058aaf489894d6c468572b
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-09-13 08:19:16 +0100

    fix(deps): update dependencies (#103)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -20,7 +20,7 @@ locals {
 
 module "event_streams" {
   source            = "terraform-ibm-modules/event-streams/ibm"
-  version           = "4.1.5"
+  version           = "4.1.6"
   es_name           = "${var.prefix}-eventsteams"
   tags              = var.resource_tags
   region            = var.region
