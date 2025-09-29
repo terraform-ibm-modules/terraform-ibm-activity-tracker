8d59855187202dd06d8072e38ca9e150645ec3cc
ocofaigh <ocofaigh@ie.ibm.com>
2025-09-29 19:12:40 +0100

    fix tests

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/IBM/go-sdk-core/v5/core"
 	"github.com/gruntwork-io/terratest/modules/files"
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
