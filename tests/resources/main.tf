241528e2a03b2f6c84ed70bdcffc96a9bb9cdf0e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-24 19:41:25 +0000

    fix(deps): update dependencies

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.5.0"
+  version           = "10.5.1"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -34,7 +34,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.5.0"
+  version = "10.5.1"
   bucket_configs = [
     {
       bucket_name            = local.logs_bucket_name
@@ -57,7 +57,7 @@ module "buckets" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.9.2"
+  version           = "1.9.3"
   instance_name     = var.prefix
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
