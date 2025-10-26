e6da0435d6a6135bc8e440c58d4a0161f19812a2
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-26 08:39:58 +0000

    chore(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.9.6

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -57,7 +57,7 @@ module "buckets" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.9.5"
+  version           = "1.9.6"
   instance_name     = var.prefix
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
