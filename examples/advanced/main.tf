09d622aaf8d9152739ad59e092dea00dd766e99d
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-11-07 20:15:46 +0000

    chore(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.9.6 (#127)
    Co-authored-by: Md Anam Raihan <md.anam.raihan17@gmail.com>

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.9.5"
+  version           = "1.9.6"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   data_storage = {
