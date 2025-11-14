628969501a28758b28088b8542785de839e73e8b
mounicatellabati <78633728+mounicatellabati@users.noreply.github.com>
2025-11-14 09:51:28 +0530

    feat: Added `next steps` url for Activity tracker event routing DA (#133)
    * feat: Added next steps url for Activity tracker event routing
    
    * feat: Added next steps url for Activity tracker event routing
    
    * feat: Added next steps url for Activity tracker event routing
    
    * Update solutions/event-routing-account-settings/outputs.tf
    
    Co-authored-by: Md Anam Raihan <md.anam.raihan17@gmail.com>
    
    * fix: Updated the description
    
    * fix: fixing indentation issues
    
    * fix: fixing indentation issues
    
    * fix: updated the description
    
    * fix: updated the label and description
    
    * fix: updated the description
    
    ---------
    
    Co-authored-by: raagamounica <mounica.tellabati@ibm.com>
    Co-authored-by: Md Anam Raihan <md.anam.raihan17@gmail.com>

--- solutions/event-routing-account-settings/outputs.tf
+++ solutions/event-routing-account-settings/outputs.tf
@@ -1,3 +1,33 @@
 ##############################################################################
 # Outputs
 ##############################################################################
+##############################################################################
+# Activity Tracker Event Routing Account Settings Next Steps URLs outputs
+##############################################################################
+
+output "next_steps_text" {
+  value       = "Activity Tracker Event Routing account settings are configured."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to Activity Tracker Event Routing overview"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/observability/event-routing/overview"
+  description = "Primary URL"
+}
+
+output "next_step_secondary_label" {
+  value       = "Learn more about Activity Tracker Event Routing account settings"
+  description = "Secondary label"
+}
+
+output "next_step_secondary_url" {
+  value       = "https://cloud.ibm.com/docs/atracker?topic=atracker-settings&interface=ui"
+  description = "Secondary URL"
+}
+
+##############################################################################
