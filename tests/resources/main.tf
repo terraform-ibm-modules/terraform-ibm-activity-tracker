854a931b4c0ad7415b174aaecee5b374038f01a7
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-28 12:41:16 +0000

    fix(deps): update dependencies

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -57,7 +57,7 @@ module "buckets" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.10.14"
+  version           = "1.10.15"
   instance_name     = var.prefix
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
