ead72c5edc98bcac35aea23b4d855cdddd614b27
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-10 12:25:04 +0000

    chore(deps): update dependencies (#165)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.10.19"
+  version           = "1.10.20"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   data_storage = {
