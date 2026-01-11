80e205781c84ea330fd0a85c1dbd3c1262bc64a8
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-11 00:21:01 +0000

    chore(deps): update dependencies (#167)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -57,7 +57,7 @@ module "buckets" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.10.20"
+  version           = "1.10.21"
   instance_name     = var.prefix
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
