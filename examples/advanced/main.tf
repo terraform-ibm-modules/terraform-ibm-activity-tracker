7d8e0020b989c88bc90c6c4aa93fd29487319c5b
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-25 04:22:31 +0000

    fix(deps): update dependencies (#176)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.10.27"
+  version           = "1.10.28"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   data_storage = {
@@ -91,7 +91,7 @@ module "key_protect" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.9.3"
+  version           = "10.9.4"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -104,7 +104,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.9.3"
+  version = "10.9.4"
   bucket_configs = [
     {
       bucket_name                   = local.at_bucket_name
