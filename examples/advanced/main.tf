4b8292668ff535d8e2ecddfbfe252780ce215cd5
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-05-30 11:41:28 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.13.9"
+  version           = "1.13.10"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   data_storage = {
@@ -182,7 +182,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 # This zone will be referenced in CBR rules for all target services (COS, Cloud Logs, Event Streams)
 module "cbr_zone_atracker" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.36.4"
+  version          = "1.36.5"
   name             = "${var.prefix}-atracker-zone"
   zone_description = "CBR Network zone for Activity Tracker Event Routing service"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
